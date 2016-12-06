5c858870e6aa6f90136c8b049a27551ebc9740d1
Plínio Naves <pliniopjn@hotmail.com>
2016-12-06 20:35:41 -0200

    tasks CRUD

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,11 +2,73 @@ module.exports = app => {
 
     const Tasks = app.libs.db.models.Tasks;
 
-    app.get('/tasks', (req, res) => {
-        Tasks.findAll({})
-            .then(tasks => {
-                res.json(tasks);
-            });
-    });
+    app.route('/tasks')
+
+        .all((req, res, next) => {
+            if (req.body) delete req.body.id;
+            next();
+        })
+
+        .get((req, res) => {
+            Tasks.findAll({})
+                .then(tasks => res.json(tasks))
+                .catch(error => {
+                    res.status(412).json({
+                        message: error.message || error
+                    });
+                })
+        })
+
+        .post((req, res) => {
+            Tasks.create(req.body)
+                .then(task => res.json(task))
+                .catch(error => {
+                    res.status(412).json({
+                        message: error.message || error
+                    })
+                });
+        });
+
+    app.route('/tasks/:id')
+
+        .all((req, res, next) => {
+            if (req.body) delete req.body.id;
+            next();
+        })
+
+        .get((req, res) => {
+            Tasks.findOne({where: req.params})
+                .then(task => {
+                    if (task) {
+                        res.json(task);
+                    } else {
+                        res.sendStatus(404);
+                    }
+                }).catch(error => {
+                    res.json({
+                        message: error.message || error
+                    })
+                });
+        })
+
+        .put((req, res) => {
+            Tasks.update(rq.body, {where: req.params})
+                .then(result => res.sendStatus(204))
+                .catch(error => {
+                    res.status(412).json({
+                        message: error.message || error
+                    });
+                });
+        })
+
+        .delete((req, res) => {
+            Tasks.delete({where: req.params})
+                .then(result => {res.sendStatus(204)})
+                .catch(error => {
+                    res.status(412).json({
+                        message: error.message || message
+                    });
+                });
+        });
 
 };
