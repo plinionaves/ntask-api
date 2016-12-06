5c6aa878047903ae0928273557ed558010c7c8ad
Plínio Naves <pliniopjn@hotmail.com>
2016-12-06 14:54:05 -0200

    returning static resource in /tasks route

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const PORT = 3000;
 
 const app = express();
 
+app.set('json spaces', 4);
+
 app.get('/', (req, res) => {
     res.json({status: 'NTask API'});
+});
+
+app.get('/tasks', (req, res) => {
+    res.json([
+        {title: 'Aprender Node.js'},
+        {title: 'Aprender Angular2'}
+    ]);
 });
 
 app.listen(PORT, () => {
