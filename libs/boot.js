2cb4ca12df11b435520fd0d2b5c44e7548a9c8e4
Plínio Naves <pliniopjn@hotmail.com>
2016-12-08 16:06:10 -0200

    add ssl for use https

--- libs/boot.js
+++ libs/boot.js
@@ -1,12 +1,22 @@
+import https from 'https';
+import fs from 'fs';
+
 module.exports = app => {
+
+    const credentials = {
+        key: fs.readFileSync("ntask.key", "utf8").toString(),
+        cert: fs.readFileSync("ntask.cert", "utf8").toString()
+    }
 
     const env = process.env.NODE_ENV;
 
     if (!env || env.trim() !== 'test') {
+        
         app.libs.db.sequelize.sync().done(() => {
-            app.listen(app.get('port'), () => {
-                console.log(`NTask API rodando na porta ${app.get('port')}...`);
-            });
+            https.createServer(credentials, app)
+                .listen(app.get('port'), () => {
+                    console.log(`NTask API rodando na porta ${app.get('port')}...`);
+                });
         });
     }
 
