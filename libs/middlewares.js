ecc3181c5b57f09b6f11fb6c2f56e49eb31dc40f
Plínio Naves <pliniopjn@hotmail.com>
2016-12-08 03:10:23 -0200

    add cors

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,9 +1,15 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
 
 module.exports = app => {
     app.set('port', 3000);
     app.set('json spaces', 4);
+    app.use(cors({
+        origin: ['http://localhost:4200'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }));
     app.use(bodyParser.json());
     app.use(app.libs.auth.initialize());
     app.use((req, res, next) => {
